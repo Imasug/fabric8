9787df870e44cd5af6c67fd8a32ec75ff2e5e4b9
suguru.imanaga <suguru.imanaga@ctc-g.co.jp>
2020-04-06 14:45:26 +0900

    Add project

---
+++ src/main/java/jp/co/ocp/sample/fabric8/Fabric8Application.java
@@ -0,0 +1,13 @@
+package jp.co.ocp.sample.fabric8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fabric8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fabric8Application.class, args);
+	}
+
+}
---
+++ src/test/java/jp/co/ocp/sample/fabric8/Fabric8ApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.ocp.sample.fabric8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fabric8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
